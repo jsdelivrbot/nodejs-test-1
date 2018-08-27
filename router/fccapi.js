4ce6a2dcd27473f733c5e4173c1f97bfc990d9e5
bach.nguyen <bach.nguyen@crystal-techs.com>
2018-08-27 09:40:33 +0700

    add whoami fcc api

--- router/fccapi.js
+++ router/fccapi.js
@@ -5,7 +5,9 @@ var router = express.Router();
 router.get('/fccapi/timestamp', function (req, res, next) {
     res.render('fccapi/timestamp');
 });
-
+router.get('/fccapi/whoami', function (req, res, next) {
+    res.render('fccapi/whoami');
+});
 router.get("/api/timestamp/:date_string", (req, res) => {
     let date = new Date(req.params.date_string.toString());
     res.json({
@@ -13,5 +15,13 @@ router.get("/api/timestamp/:date_string", (req, res) => {
         utc: date.toUTCString()
     })
 })
+
+router.get("/api/whoami", function (req, res) {
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
+    })
+});
 
 module.exports = router;
